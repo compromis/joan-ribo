eff7b260980bcbfbafff05ac123c046455f0802c
Guillem <guillem@disedit.com>
2019-03-22 17:03:06 +0100

    Add fonts and bootstrap
    Also remove unnecessary starter code

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,18 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Oswald`,
+            subsets: [`latin`],
+          }
+        ],
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
