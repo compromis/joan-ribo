4f7e4ea94c618c45220a6e89eb2bc44464b6db79
Guillem <guillem@disedit.com>
2019-03-29 10:15:05 +0100

    Update subscribe link on whatsapp component

--- src/components/whatsapp.js
+++ src/components/whatsapp.js
@@ -11,7 +11,7 @@ const Whatsapp = () => (
             <h2><FormattedMessage id="whatsapp_header" /></h2>
             <h3 className="mb-4"><FormattedMessage id="whatsapp_text" /></h3>
             <p><FormattedMessage id="whatsapp_instructions" /></p>
-            <a href="https://api.whatsapp.com/send?phone=34607812839&text=Vull%20rebre%20informaci%C3%B3%20de%20Comprom%C3%ADs+Val%C3%A8ncia%20al%20meu%20WhatsApp&source=&data=" className={'whatsapp-button'}><FaWhatsapp className="icon" /> <FormattedMessage id="whatsapp_cta" /></a>
+            <a href="https://api.whatsapp.com/send?l=ca&phone=34607812839&text=Vull%20rebre%20informaci%C3%B3%20de%20Comprom%C3%ADs%20Val%C3%A8ncia%20al%20meu%20WhatsApp&source=&data=" className={'whatsapp-button'}><FaWhatsapp className="icon" /> <FormattedMessage id="whatsapp_cta" /></a>
         </div>
       </Fade>
     </div>
