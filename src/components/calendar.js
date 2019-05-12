a694688cec7399e84789048512da601c5a9ec1a1
Guillem <guillem@disedit.com>
2019-05-12 10:40:16 +0200

    Update calendar style

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -4,7 +4,7 @@ import { FormattedMessage } from "react-intl";
 const Calendar = () => (
   <section className='container calendar' id='calendar'>
     <h2><FormattedMessage id="events_header"/></h2>
-    <iframe title="Actes" src="https://calendar.google.com/calendar/embed?src=g9f425v99hhkneef0rlee1qvh8%40group.calendar.google.com&ctz=Europe%2FMadrid" style={{border: '0', padding: '30px;'}} width="100%" height={600} frameBorder={0} scrolling="no"></iframe>
+    <iframe title="Actes" src="https://calendar.google.com/calendar/embed?src=g9f425v99hhkneef0rlee1qvh8%40group.calendar.google.com&ctz=Europe%2FMadrid" width="100%" height={600} frameBorder={0} scrolling="no"></iframe>
   </section>
 )
 
